0d93ad65ed0b1124a5755e12a99c0503baa6538f
smvasconcelos <smvasconcelos11@gmail.com>
2022-10-06 10:50:23 -0300

    alteraçoes basicas

--- lexor.c
+++ lexor.c
@@ -59,6 +59,13 @@ int consume_extras(char);
 // Printa o estado da executação
 void print_state(int, char);
 
+
+/*
+ * Um pi com número p0 indica que o char no topo da pilha e o que está sendo lido, irá ser consumido
+ * ou seja, representa uma transição x.
+ * Para executar o programa é necessário passar o nome do arquivo pelo argv saida.exe input.txt
+ * no arquivo presente existem várias palavras, somente a primeira linha será lida.
+*/
 int main (int argc, char *argv[ ])
 {
 	char token;
@@ -79,11 +86,11 @@ int main (int argc, char *argv[ ])
 				goto fim;
 			}
 
-			if(consume_extras(token)){goto q1;}
-			else if(p1_p6(token)) {goto q1;}
-			else if(p5_p11(token)) {goto q1;}
-			else if(p12_p15(token)) {goto q1;}
-			else if(p16_p24(token)) {goto q1;}
+			if(consume_extras(token))goto q1;
+			else if(p1_p6(token)) goto q1;
+			else if(p5_p11(token)) goto q1;
+			else if(p12_p15(token)) goto q1;
+			else if(p16_p24(token)) goto q1;
 			else goto fim;
 
 
@@ -93,9 +100,9 @@ int main (int argc, char *argv[ ])
 				puts("\n A palavra foi consumida totalmente.");
 			else{
 				puts("*****************************************************************************************************************");
-				print_state(-1, token);
+				print_state(-1, token == '\n' ? '\0' : token);
 				puts("*****************************************************************************************************************");
-				puts("\n A palavra não pode ser consumida.");
+				puts("\n A palavra nao pode ser consumida.");
 			}
 
 	}else{
@@ -153,53 +160,52 @@ int p1_p6(char token){
 
 int p5_p11(char token)
 {
-		/*
-			! p7: E → 0
-			! p7: E -> 1
-			! p9: E -> x
-			! p10: E -> y
-			! p11: E -> (EXE)
-		*/
+	/*
+		! p7: E → 0
+		! p7: E -> 1
+		! p9: E -> x
+		! p10: E -> y
+		! p11: E -> (EXE)
+	*/
 
 
-		// Empilha
-		if(token == '0' && STACK[TOP] == 'E') { PI = 7; print_state(1, token);  pop(); push('0'); return 1;}
-		else if(token == '1' && STACK[TOP] == 'E') { PI = 8; print_state(1, token);  pop(); push('1'); return 1;}
-		else if(token == 'x' && STACK[TOP] == 'E') { PI = 9; print_state(1, token);  pop(); push('x'); return 1;}
-		else if(token == 'y' && STACK[TOP] == 'E') { PI = 10; print_state(1, token);  pop(); push('y'); return 1;}
-		else if((token == '(' && STACK[TOP] == 'E')) { PI = 11; print_state(1, token);  pop(); push(')'); push('E'); push('X'); push('E'); push('('); return 1;}
-		// Consume
-		else if(token == '0' && STACK[TOP] == '0') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == '1' && STACK[TOP] == '1') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == 'x' && STACK[TOP] == 'x') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == 'y' && STACK[TOP] == 'y') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		return 0;
+	// Empilha
+	if(token == '0' && STACK[TOP] == 'E') { PI = 7; print_state(1, token);  pop(); push('0'); return 1;}
+	else if(token == '1' && STACK[TOP] == 'E') { PI = 8; print_state(1, token);  pop(); push('1'); return 1;}
+	else if(token == 'x' && STACK[TOP] == 'E') { PI = 9; print_state(1, token);  pop(); push('x'); return 1;}
+	else if(token == 'y' && STACK[TOP] == 'E') { PI = 10; print_state(1, token);  pop(); push('y'); return 1;}
+	else if((token == '(' && STACK[TOP] == 'E')) { PI = 11; print_state(1, token);  pop(); push(')'); push('E'); push('X'); push('E'); push('('); return 1;}
+	// Consume
+	else if(token == '0' && STACK[TOP] == '0') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == '1' && STACK[TOP] == '1') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == 'x' && STACK[TOP] == 'x') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == 'y' && STACK[TOP] == 'y') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	return 0;
 }
 
 int p12_p15(char token)
 {
-		/*
-		! p12: X → +
-		! p13: X -> -
-		! p14: X -> *
-		! p15: X -> /
-		*/
-		// Empilha
-		if(token == '+' && STACK[TOP] == 'X') { PI = 12; print_state(1, token); pop(); push('+'); return 1;}
-		else if(token == '-' && STACK[TOP] == 'X') { PI = 13; print_state(1, token); pop(); push('-'); return 1;}
-		else if(token == '*' && STACK[TOP] == 'X') { PI = 14; print_state(1, token); pop(); push('*'); return 1;}
-		else if(token == '/' && STACK[TOP] == 'X') { PI = 15; print_state(1, token); pop(); push('/'); return 1;}
-		// Consome
-		else if(token == '+' && STACK[TOP] == '+') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == '-' && STACK[TOP] == '-') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == '*' && STACK[TOP] == '*') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		else if(token == '/' && STACK[TOP] == '/') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
-		return 0;
+	/*
+	! p12: X → +
+	! p13: X -> -
+	! p14: X -> *
+	! p15: X -> /
+	*/
+	// Empilha
+	if(token == '+' && STACK[TOP] == 'X') { PI = 12; print_state(1, token); pop(); push('+'); return 1;}
+	else if(token == '-' && STACK[TOP] == 'X') { PI = 13; print_state(1, token); pop(); push('-'); return 1;}
+	else if(token == '*' && STACK[TOP] == 'X') { PI = 14; print_state(1, token); pop(); push('*'); return 1;}
+	else if(token == '/' && STACK[TOP] == 'X') { PI = 15; print_state(1, token); pop(); push('/'); return 1;}
+	// Consome
+	else if(token == '+' && STACK[TOP] == '+') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == '-' && STACK[TOP] == '-') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == '*' && STACK[TOP] == '*') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	else if(token == '/' && STACK[TOP] == '/') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
+	return 0;
 }
 
 int p16_p24(char token)
 {
-	// Não sei ainda
 	/*
 	! p16: C → h=E
 	! p17: C -> i=E
@@ -212,16 +218,16 @@ int p16_p24(char token)
 	! p24: C -> o(E; E; E){ C; }
 	*/
 	// Empilha
-	if(token == 'h' && STACK[TOP] == 'C') { PI = 16; pop(); push('E');  push('='); push('h'); return 1;}
-	else if(token == 'j' && STACK[TOP] == 'C') { PI = 18; pop(); push('E');  push('='); push('j'); return 1;}
-	else if(token == 'i' && STACK[TOP] == 'C') { PI = 17; pop(); push('E');  push('='); push('i'); return 1;}
-	else if(token == 'k' && STACK[TOP] == 'C') { PI = 19; pop(); push('E');  push('='); push('k'); return 1;}
-	else if(token == 'z' && STACK[TOP] == 'C') { PI = 20; pop(); push('E');  push('='); push('z'); return 1;}
+	if(token == 'h' && STACK[TOP] == 'C') { PI = 16; print_state(1, token); pop(); push('E');  push('='); push('h'); return 1;}
+	else if(token == 'j' && STACK[TOP] == 'C') { PI = 18; print_state(1, token); pop(); push('E');  push('='); push('j'); return 1;}
+	else if(token == 'i' && STACK[TOP] == 'C') { PI = 17; print_state(1, token); pop(); push('E');  push('='); push('i'); return 1;}
+	else if(token == 'k' && STACK[TOP] == 'C') { PI = 19; print_state(1, token); pop(); push('E');  push('='); push('k'); return 1;}
+	else if(token == 'z' && STACK[TOP] == 'C') { PI = 20; print_state(1, token); pop(); push('E');  push('='); push('z'); return 1;}
 	// p21-p24
-	else if(token == '(' && STACK[TOP] == 'C') { PI = 21; pop(); push(')');  push('E'); push('X'); push('E'); push('('); return 1;}
-	else if(token == 'w' && STACK[TOP] == 'C') { PI = 22; pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push('('); push('w'); return 1;}
-	else if(token == 'f' && STACK[TOP] == 'C') { PI = 23; pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push('('); push('f'); return 1;}
-	else if(token == 'o' && STACK[TOP] == 'C') { PI = 24; pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push(';'); push('E'); push(';'); push('E'); push('('); push('o'); return 1;}
+	else if(token == '(' && STACK[TOP] == 'C') { PI = 21; print_state(1, token); pop(); push(')');  push('E'); push('X'); push('E'); push('('); return 1;}
+	else if(token == 'w' && STACK[TOP] == 'C') { PI = 22; print_state(1, token); pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push('('); push('w'); return 1;}
+	else if(token == 'f' && STACK[TOP] == 'C') { PI = 23; print_state(1, token); pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push('('); push('f'); return 1;}
+	else if(token == 'o' && STACK[TOP] == 'C') { PI = 24; print_state(1, token); pop(); push('}'); push(';'); push('C'); push('{'); push(')'); push('E'); push(';'); push('E'); push(';'); push('E'); push('('); push('o'); return 1;}
 	// Consome
 	else if(token == 'w' && STACK[TOP] == 'w') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
 	else if(token == 'o' && STACK[TOP] == 'o') {print_state(1, token);  pop(); TOKEN_POS++; return 1;}
@@ -270,4 +276,12 @@ void print_state(int state, char token)
 {
 	printf("Step: %2d \t Estado: q%3d \t Token: %5c \t Stack: %20s \t\t pi: p%2d \t Word: %s", STEP, state,token, STACK, PI, WORD);
 	PI = 0;
+}
+
+void show()
+{
+	for(int i = 0; i < TOP; i++)
+	{
+		printf("Index: %1d \t Stack: %1c\n", i, STACK[i]);
+	}
 }
